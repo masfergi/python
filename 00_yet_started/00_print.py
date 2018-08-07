029e470f5c629741774ef7fa14cad7039c8d16c3
masfergi <fesubu7@gmail.com>
2018-08-08 01:49:48 +0700

    Date and Month Added
    Wednesday, August 8 2018

--- 00_yet_started/00_print.py
+++ 00_yet_started/00_print.py
@@ -1,3 +1,7 @@
+'''------------------------
+Wednesday, August 8 2018
+------------------------'''
+
 pesan_1 = "Hello World"
 pesan_2 = ", I'll start learning python from now."
 print(pesan_1+pesan_2)

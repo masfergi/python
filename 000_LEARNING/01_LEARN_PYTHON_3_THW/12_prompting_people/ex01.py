22759519fb592dd0ca63ac6274e74fd14b26a397
masfergi <fesubu7@gmail.com>
2018-08-08 23:15:08 +0700

    exercise 10 - exercise 12

---
+++ 000_LEARNING/01_LEARN_PYTHON_3_THW/12_prompting_people/ex01.py
@@ -0,0 +1,7 @@
+# melakukan sebuah input dengan sebuah string di print sebelum input di masukkan
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weight? ")
+
+# "{}" hanya mengeksekusi nilai bukan string
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
